a1d45da72670c2d8b88894093305d93152233877
mntmn <lukas@mntmn.com>
2020-07-19 16:57:42 +0200

    fix schema migration script

--- generate_schema_and_models.sh
+++ generate_schema_and_models.sh
@@ -2,7 +2,7 @@
 
 # this drops all tables!
 # and generates src/schema.rs
-diesel redo
+diesel migration redo
 
 # this generates src/models.rs
 diesel_ext -d 'serde::Serialize, Queryable, Insertable, Identifiable' -I 'crate::schema::*' > src/models.rs
