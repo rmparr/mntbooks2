d16bbe343c0e0c2c53672292359ab45f2c19df31
mntmn <lukas@mntmn.com>
2020-09-18 12:29:46 +0200

    legacy import: fix documents cleanup

--- scripts/mntbooks-legacy-import.sql
+++ scripts/mntbooks-legacy-import.sql
@@ -7,4 +7,10 @@ delete from documents;
 .import mntbooks-legacy-export-bookingdocs.csv booking_docs
 .import mntbooks-legacy-export-docimages.csv document_images
 .import mntbooks-legacy-export-documents.csv documents
+
+-- clean up and re-link invoices
+update booking_docs set doc_id = (select documents.id from documents where booking_docs.doc_id like "invoice-%-"||documents.id||".pdf") where doc_id like "invoice-%";
+update document_images set doc_id = (select documents.id from documents where document_images.doc_id like "invoice-%-"||documents.id||".pdf") where doc_id like "invoice-%";
+delete from documents where id like "invoice-%" and kind="receipt";
+
 .quit
